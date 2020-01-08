bb0816e06275113edf2bda8b91eac9698d075d40
James Stuart <james.stuart@gmail.com>
2020-01-08 15:01:18 -0500

    removed welcome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  def welcome
-    render component: 'Welcome', props: { greeting: 'hello! ' }
-  end
 end
--- app/javascript/components/ScriptEditor/Store/Models/Script.ts
+++ app/javascript/components/ScriptEditor/Store/Models/Script.ts
@@ -1,15 +1,17 @@
-import { action, Action } from 'easy-peasy';
+import { action, Action, computed, Computed } from 'easy-peasy';
 
 export interface ScriptModel {
   setData: Action<ScriptModel, string>;
   data: string;
+  compilationResult: <
 }
 
 const scriptModel: ScriptModel = {
   setData: action((state, data) => {
     state.data = data;
   }),
-  data: "",
+  compilationResult:
+    data: "",
 };
 
 export default scriptModel;
