b2acea00a9955f2ed5f46ba3da49eaaeb2145737
James Stuart <james.stuart@gmail.com>
2020-01-08 14:46:30 -0500

    added blockview

---
+++ app/javascript/components/ScriptEditor/components/BlockView.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-markdown";
+import "ace-builds/src-noconflict/theme-github";
+
+import { useStoreActions, useStoreState } from '../hooks'
+
+interface IEditorProps {
+  initialData: string;
+}
+
+const Editor: React.FC<IEditorProps> = ({ initialData }) => {
+  const setData = useStoreActions(actions => actions.script.setData);
+  const data = useStoreState(state => state.script.data);
+
+  useEffect(() => { setData(initialData) }, []);
+
+  return (
+    <div>
+      <input type="hidden" id="script_data" name="script[data]" value={data} />
+      <AceEditor
+        mode="markdown"
+        theme="github"
+        name="ScriptEditor"
+        value={data}
+        onChange={setData}
+      />
+    </div>
+  );
+}
+
+export default Editor
--- app/javascript/components/ScriptEditor/index.tsx
+++ app/javascript/components/ScriptEditor/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { StoreProvider } from 'easy-peasy';
 import scriptEditorStore from './Store'
 import Editor from './components/Editor';
+import BlockView from './components/BlockView';
 
 interface IScriptEditorProps {
   initialData: string;
@@ -12,6 +13,7 @@ const ScriptEditor: React.FC<IScriptEditorProps> = ({ initialData }) => {
   return (
     <StoreProvider store={scriptEditorStore}>
       <Editor initialData={initialData} />
+      <BlockView />
     </StoreProvider>
   );
 }
