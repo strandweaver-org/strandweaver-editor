39a0472014cc6b49164eaf9b68bf2ed375af826e
James Stuart <james.stuart@gmail.com>
2020-01-09 12:33:38 -0500

    removed test, added spec

--- app/javascript/components/ScriptEditor/Store/Models/Script.ts
+++ app/javascript/components/ScriptEditor/Store/Models/Script.ts
@@ -1,17 +1,18 @@
 import { action, Action, computed, Computed } from 'easy-peasy';
+import Engine from 'strandweaver-machine'
 
 export interface ScriptModel {
   setData: Action<ScriptModel, string>;
   data: string;
-  compilationResult: <
+  compilationResult: Computed<ScriptModel, number>;
 }
 
 const scriptModel: ScriptModel = {
   setData: action((state, data) => {
     state.data = data;
   }),
-  compilationResult:
-    data: "",
+  compilationResult: computed(state => state.data.length),
+  data: "",
 };
 
 export default scriptModel;
--- app/javascript/components/ScriptEditor/components/BlockView.tsx
+++ app/javascript/components/ScriptEditor/components/BlockView.tsx
@@ -1,33 +1,16 @@
-import React, { useEffect } from "react";
-import AceEditor from "react-ace";
+import React from "react";
 
-import "ace-builds/src-noconflict/mode-markdown";
-import "ace-builds/src-noconflict/theme-github";
+import { useStoreState } from '../hooks'
 
-import { useStoreActions, useStoreState } from '../hooks'
 
-interface IEditorProps {
-  initialData: string;
-}
-
-const Editor: React.FC<IEditorProps> = ({ initialData }) => {
-  const setData = useStoreActions(actions => actions.script.setData);
+const BlockView: React.FC = () => {
   const data = useStoreState(state => state.script.data);
 
-  useEffect(() => { setData(initialData) }, []);
 
   return (
     <div>
-      <input type="hidden" id="script_data" name="script[data]" value={data} />
-      <AceEditor
-        mode="markdown"
-        theme="github"
-        name="ScriptEditor"
-        value={data}
-        onChange={setData}
-      />
     </div>
   );
 }
 
-export default Editor
+export default BlockView
