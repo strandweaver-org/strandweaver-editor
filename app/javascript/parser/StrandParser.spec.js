954851140e4468dace8e78790f6f8124b763e305
James Stuart <james.stuart@gmail.com>
2020-01-09 12:48:41 -0500

    added barebones test

---
+++ app/javascript/parser/StrandParser.spec.js
@@ -0,0 +1,19 @@
+import StrandLanguage from "./StrandLanguage";
+
+it("given plain text", () => {
+  const text = `\
+block:
+  alpha
+  bravo
+  block:
+         charlie
+         delta
+         echo
+         block:
+          foxtrot
+  golf
+`;
+  const res = StrandLanguage.Statement.parse(text);
+
+  expect(res).toBe("x");
+});
