680680ca611a4ad8dba4c916dda7ede317d46247
James Stuart <james.stuart@gmail.com>
2020-01-27 18:52:20 -0500

    parses choices right

--- spec/javascript/Language/Parser/choices.spec.ts
+++ spec/javascript/Language/Parser/choices.spec.ts
@@ -10,7 +10,7 @@ describe("choices", () => {
 You stare at the menu # beep boop # bick
 * order the grilled cheese # beep # brap
   The grilled cheese is delicious. // is it tho?
-* order the menu
+* eat the menu
   It's not tasty. Also people give you weird looks.
 `);
 
@@ -18,8 +18,8 @@ You stare at the menu # beep boop # bick
 
     expect(res.tokens[0]).toBeATokenOfType('Paragraph');
     expect(res.tokens[0].getTags()).toStrictEqual(["beep boop", "bick"])
-    expect(res.tokens[1]).toBeATokenOfType('Choice');
 
+    expect(res.tokens[1]).toBeATokenOfType('Choice');
     const choice = (res.tokens[1] as tokens.Choice);
     expect(choice.text).toBe("order the grilled cheese");
     expect(choice.getTags()).toStrictEqual(["beep", "brap"])
@@ -27,6 +27,12 @@ You stare at the menu # beep boop # bick
     const paragraph = (res.tokens[2] as tokens.Paragraph);
     expect(paragraph.text).toBe("The grilled cheese is delicious.")
     expect(paragraph.indentLevel).toBe(2);
+
+    expect(res.tokens[3]).toBeATokenOfType('Choice');
+    const choice2 = (res.tokens[3] as tokens.Choice);
+    expect(choice2.text).toBe("eat the menu");
+
+
   });
 
 
